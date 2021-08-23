1dd4d6e26aacd4e4c14e9caf41a1c44c8cf34428
Daniel Huisman <daniel@huisman.me>
2021-08-23 17:12:43 +0200

    Add task initialization

--- corporations/apps.py
+++ corporations/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db import connection
 
 
 class CorporationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'corporations'
+
+    def ready(self):
+        if 'django_q_schedule' in connection.introspection.table_names():
+            from .tasks import initialize_tasks
+            initialize_tasks()
